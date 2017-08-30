b7aeead7b91ef47b9cceaaa9a31c5a222a93f7d8
Cassi Gallagher <email": null>
2017-08-29 17:26:37 -0700

    Add API information to server file

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const request = require('request')
+const apiKey = key.env.API_Key
+const apiSecret = key.env.API_Secret
+
+require('dotenv').config()
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+app.use(bodyParser).urlencoded({ extended: true })
 
 app.get('/', function (req, res) {
+  res.render('index')
+})
+
+app.post('/', function (req, res) {
+  console.log(req.body.animal)
   res.render('index')
 })
 

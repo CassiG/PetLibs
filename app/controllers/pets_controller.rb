1400d332c48c187e874ff74111ae1556b308d429
Cassi Gallagher <ccgall@gmail.com>
2017-10-16 14:46:25 -0700

    Add photo and description data in the parsed homepage list

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -4,11 +4,15 @@ require 'dotenv/load'
 class PetsController < ApplicationController
 
   def index
-    petData = 'http://api.petfinder.com/pet.find?key='+ ENV['PETFINDER_KEY'] + '&animal=dog&location=98133&format=json'
+    p params["type"]
+    petData = 'http://api.petfinder.com/pet.find?key=' + ENV['PETFINDER_KEY'] + '&animal=' + params["type"] + '&location=98133&format=json'
+
     response = HTTParty.get(petData)
     @pet = response.parsed_response
     @pets = @pet['petfinder']['pets']['pet']
 
+    p @pets
+    p '*******************'
 
   end
 

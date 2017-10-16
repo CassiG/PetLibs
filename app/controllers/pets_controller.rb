863b2f32380cb799a72a0e9fd19c8698221a942d
Cassi Gallagher <ccgall@gmail.com>
2017-10-16 16:35:10 -0700

    Add description check in get call for description

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -4,16 +4,19 @@ require 'dotenv/load'
 class PetsController < ApplicationController
 
   def index
-    p params["type"]
     petData = 'http://api.petfinder.com/pet.find?key=' + ENV['PETFINDER_KEY'] + '&animal=' + params["type"] + '&location=98133&format=json'
 
     response = HTTParty.get(petData)
     @pet = response.parsed_response
     @pets = @pet['petfinder']['pets']['pet']
 
-    p @pets
-    p '*******************'
+    @petList = []
 
+    @pets.each do |pet|
+      if pet['description']['$t'].include?(" #{params['description']} ")
+        @petList << pet
+      end
+    end
   end
 
 end

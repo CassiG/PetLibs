1cffaeea180c5c6c45ccd5679333405c22958931
Cassi Gallagher <ccgall@gmail.com>
2017-10-16 19:20:12 -0700

    Add additional parameters for search function on homepage

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -4,26 +4,30 @@ require 'dotenv/load'
 class PetsController < ApplicationController
 
   def index
-    p params
     petData = 'http://api.petfinder.com/pet.find?key=' + ENV['PETFINDER_KEY'] + '&animal=' + params["animal"] + + '&size=' + params["animal-size"] + '&location=98133&format=json'
 
     response = HTTParty.get(petData)
     @pet = response.parsed_response
     @pets = @pet['petfinder']['pets']['pet']
+    @petNameList = []
+    @petDescriptionList = []
+    @petFoodList = []
     @petList = []
 
     @pets.each do |pet|
-      if pet['name']['$t'].include?(" #{params['name']}  ") ||
-         pet['description']['$t'].include?(" #{params['activity1']}  ") ||
-         pet['description']['$t'].include?(" #{params['activity2']}  ") ||
-         pet['description']['$t'].include?(" #{params['adjective']}  ") ||
-         pet['description']['$t'].include?(" #{params['food']}  ") ||
-         pet['description']['$t'].include?(" #{params['treat']}  ") ||
-         pet['description']['$t'].include?(" #{params['adjective1']}  ") ||
-         pet['description']['$t'].include?(" #{params['adjective2']}  ")
-        @petList << pet
+
+
+      if pet['description']['$t'].include?(" #{params['activity1']}  ") ||
+            pet['description']['$t'].include?(" #{params['adjective1']}  ") ||
+            pet['description']['$t'].include?(" #{params['adjective']}  ")
+        @petDescriptionList << pet
       end
+
     end
+
+    p @petNameList
+    p @petDescriptionList
+    p @petFoodList
   end
 
 end
